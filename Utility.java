ebcd74088717f07fe0a1e4b4acbe311936d9835c
abansal <abansal@overlake.org>
2023-09-14 14:56:18 -0700

    update utlity.java

--- Utility.java
+++ Utility.java
@@ -1,5 +1,10 @@
 public class Utility{
 public static void printHelloWorld(String string){
-	System.out.println(string);
+	if (string.equals("hello")) { 
+		System.out.println("Hello World");	
+	}
+	else if (string.equals("goodbye")){ 
+		System.out.println("Goodbye, cruel world!");
+	}
 }
 }
